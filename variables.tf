7d7228508a8fdcb300412a38a1e5727f5a57eff1
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-05-09 23:12:59 +0000

    update var

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "cidr_block" {
-    type = string
-   default =   ""10.0.0.0/16""
+  type    = string
+  default = "10.0.0.0/16"
 }
+
 variable "num_public_subnets" {
-    type = number
-   default =   "3"
+  type    = number
+  default = 3
 }
+
 variable "num_private_subnets" {
-    type = number
-   default =   "3"
+  type    = number
+  default = 3
 }
+
 variable "internet_gateway_name" {
-    type = string
-   default =   "my-internet-gateway"
+  type    = string
+  default = "my-internet-gateway"
 }
